ba1abd31d09f85b30a5ead2e7d9d3d19fd552cba
yeon <rladusdn56@gmail.com>
2025-03-11 00:58:12 +0900

    Fix: week2 띄어쓰기때문에 오답 수정

--- peuip/assignment2/2-3.py
+++ peuip/assignment2/2-3.py
@@ -3,4 +3,4 @@ x2, y2 = map(float, input("Enter x2 and y2: ").split())
 x= x2-x1
 y= y2-y1
 distance = (x**2 + y**2)**0.5
-print("The distance of the two points is ", distance)
+print("The distance of the two points is", distance)
